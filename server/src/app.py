c96378fb7079c45b174f0fc7e5a3e418e87f5f12
marielleregner <94092950+marielleregner@users.noreply.github.com>
2022-05-19 22:13:59 +0800

    user

--- server/src/app.py
+++ server/src/app.py
@@ -1,88 +1,29 @@
-from flask import Flask, jsonify, request, make_response, g
-from models.dog import Dog
-from models.cat import Cat
-from db import Db
+from flask import Flask, request, jsonify, g
+from v1.dog.router import DogRouter
+from v1.cat.router import CatRouter
+from v1.auth import login as auth_login, verify_token as auth_verify_token
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'I/L0ve/CIT-U'
 
-#g.db = Db()
-#g.db.connect()
+app.register_blueprint(DogRouter.handler())
+app.register_blueprint(CatRouter.handler())
 
-@app.route('/')
-def home():
-    return "Hello Flask!"
+@app.route('/v1/login', methods=['POST'])
+def login():
+    data = request.json
+    if 'username' in data and 'password' in data:
+        token = auth_login(data['username'], data['password'])
+        if token is not False:
+            return jsonify({'token': token})
+    return jsonify({'message': 'Invalid username or password'}), 403
 
-@app.route('/dogs', methods=['POST', 'GET', 'PUT', 'DELETE'])
-def dogs():
-    dog = Dog()
-    if str(request.method).upper() == 'POST':
-        resp = dog.post(request.json)
-        if resp == False:
-            return make_response(jsonify({
-                "error": "Failed to add. There are items in your request that are invalid."
-            }), 400)
-        return jsonify(resp)
-    if str(request.method).upper() == 'PUT':
-        return jsonify(dog.put(request.json))
-    if str(request.method).upper() == 'DELETE':
-        return jsonify(dog.delete(request.json))
-    dogs = dog.get()
-    return jsonify(dogs)
-
-@app.route('/dogs/<dog_id>', methods=['GET', 'PUT', 'DELETE'])
-def dog(dog_id):
-    dog_object = Dog()
-    dog = dog_object.get({"id": dog_id})
-    if dog is None:
-        return make_response(jsonify({"error": "Dog id not found."}), 404)
-    if str(request.method).upper() == 'PUT':
-        dog_data = request.json
-        dog_data['id'] = dog_id
-        resp = dog_object.put(dog_data)
-        if resp == False:
-            return make_response(jsonify({
-                "error": "Failed to update. There are items in your request that are invalid."
-            }), 400)
-        return jsonify(resp)
-    if str(request.method).upper() == 'DELETE':
-        return jsonify(dog_object.delete(dog_id))
-    return jsonify(dog)
-
-@app.route('/cats', methods=['POST', 'GET', 'PUT', 'DELETE'])
-def cats():
-    cat = Cat()
-    if str(request.method).upper() == 'POST':
-        resp = cat.post(request.json)
-        if resp == False:
-            return make_response(jsonify({
-                "error": "Failed to add. There are items in your request that are invalid."
-            }), 400)
-        return jsonify(resp)
-    if str(request.method).upper() == 'PUT':
-        return jsonify(cat.put(request.json))
-    if str(request.method).upper() == 'DELETE':
-        return jsonify(cat.delete(request.json))
-    cats = cat.get()
-    return jsonify(cats)
-
-@app.route('/cats/<cat_id>', methods=['GET', 'PUT', 'DELETE'])
-def cat(cat_id):
-    cat_object = Cat()
-    cat = cat_object.get({"id": cat_id})
-    if cat is None:
-        return make_response(jsonify({"error": "cat id not found."}), 404)
-    if str(request.method).upper() == 'PUT':
-        cat_data = request.json
-        cat_data['id'] = cat_id
-        resp = cat_object.put(cat_data)
-        if resp == False:
-            return make_response(jsonify({
-                "error": "Failed to update. There are items in your request that are invalid."
-            }), 400)
-        return jsonify(resp)
-    if str(request.method).upper() == 'DELETE':
-        return jsonify(cat_object.delete(cat_id))
-    return jsonify(cat)
+@app.route('/v1/verify-token')
+def verify_token():
+    token = request.args.get('token')
+    if not auth_verify_token(token):
+        return jsonify({'message': 'Invalid token'}), 403
+    return jsonify({'ok': 'Token is valid'})
 
 if __name__ == "__main__":
     app.run(debug=True,host='0.0.0.0',port=6000)
